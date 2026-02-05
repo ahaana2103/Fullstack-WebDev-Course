bf52458d18314684afbcbeb51078adffcc725df3
ahaana2103 <ahaanavatsa@gmail.com>
2026-02-05 14:28:07 +0530

    Added new files

---
+++ file.js
@@ -0,0 +1,18 @@
+//import function name from path
+import {add} from "./utils.js" ;
+const output= add(1, 2);
+console.log("Adding (1, 2) :" , output);
+
+import { sub } from "./functions/function.js";
+console.log("Subtracting(2, 1) :",  sub(2, 1));
+
+import fs, { readFile } from "fs";
+console.log("Start --");
+fs.readFile("file.txt", "utf8" ,(err, output) => {
+    if(err) {
+        console.log( "err --",err);
+    }
+    console.log("Output ---" ,output);
+});
+
+console.log("Ended ---");
---
+++ utils.js
@@ -0,0 +1,7 @@
+ export const add = (a, b) => {
+    return a + b;
+};
+const multi= (a, b) => {
+    return a*b;
+};
+export default multi;
