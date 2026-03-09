b797c28a4fa6bec692303adf528c94f9b72c961d
ahaana2103 <ahaanavatsa@gmail.com>
2026-03-09 14:51:01 +0530

    Updated files

---
+++ Javascript/first.js
@@ -0,0 +1,33 @@
+
+const student ={
+    fullName: "Rohan Mehra",
+    cgpa: 7.2,
+    age : 22,
+    isPass: true,
+};
+console.log(student);
+
+console.log(student["age"]);
+student["age"]= student['age'] +1;
+console.log(student["age"]);
+
+const product= {
+    name: "parker Jotter Standard CT Ball Pen(Black)",
+    rating: 4,
+    offer: 5,
+    price:220,
+};
+console.log(product);
+
+const profile= {
+    username: "@ahaana",
+    followers:100,
+    following: 90,
+    post:0,
+    isFollow:true,
+    message:"Hey...This is Ahaana Vatsa!",
+    bio:"Leraring the life",
+};
+console.log(profile);
+
+
